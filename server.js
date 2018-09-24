66c1446ccb3ab628f0e02244d3325cb7ee9fe819
Derek O'Neill <derekoneil@gmail.com>
2018-09-24 13:52:14 -0400

    check both fn urls in upload

--- server.js
+++ server.js
@@ -84,24 +84,25 @@ app.post('/upload', function(req, res) {
     }
     else if(response && response.statusCode == 404) {
     	const options2 = {
-	    method: 'POST',
-	    uri: 'http://my-release-fn-api/t/imgconvert/resize128',
-	    encoding: null,
-	    headers: {
-	      'Content-type': 'application/octet-stream'
-	    },
-	    body: sampleFile.data
-	};
-	request(options2, function(error, response, body) {
-		
-		if (response && response.statusCode == 200) {
-			res.send(body.toString('base64'));
-		}
-		else {
-		      res.status(500);
-		      res.end();
-		    }
-	}
+  	    method: 'POST',
+  	    uri: 'http://my-release-fn-api/t/imgconvert/resize128',
+  	    encoding: null,
+  	    headers: {
+  	      'Content-type': 'application/octet-stream'
+  	    },
+  	    body: sampleFile.data
+    	};
+      
+    	request(options2, function(error, response, body) {
+    		
+    		if (response && response.statusCode == 200) {
+    			res.send(body.toString('base64'));
+    		}
+    		else {
+    		      res.status(500);
+    		      res.end();
+    		    }
+    	});
     }
     else {
       res.status(500);
